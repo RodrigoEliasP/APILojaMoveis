3b25d544509a0f73b7b4215b8e68d05e0b03d0ad
RodrigoEliasP <59834571+RodrigoEliasP@users.noreply.github.com>
2020-09-17 15:52:27 -0300

    help

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ const products = require('./assets/furniture.json');
 const cors = require('cors');
 
 const app = express();
-app.use(express.json());
+
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req,res)=>{
     res.send({message: "oi"});
