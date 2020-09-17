e4c6cb70bca7960905681fe1df0d4059a9f65d5d
RodrigoEliasP <59834571+RodrigoEliasP@users.noreply.github.com>
2020-09-17 15:14:48 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const { response } = require('express');
+const express =  require('express');
+const login = require('./assets/user.json');
+const products = require('./assets/furniture.json');
+
+const app = express(express.json());
+
+
+app.get('/user', (req,res)=>{
+    res.send(login);
+});
+
+app.get('/products', (req,res)=>{
+    res.send(products);
+});
+
+app.get('/product/:id', (req,res)=>{
+    const {id} = req.params;
+
+    const index = products.findIndex(p => p.id == id);
+
+    if(index !== -1){
+        res.send(products[index]);
+    }else{
+        res.sendStatus(404);
+    }
+});
+
+app.listen(8080, ()=>{
+    console.log('api on');
+});
